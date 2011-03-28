39edfe21ecb0781f947854e0f7da2b7c168d5541
Greg Heartsfield <scsibug@imap.cc>
2011-03-28 18:26:40 -0500

    Removed chat module (not being used).

--- chat.js
+++
@@ -1,8 +0,0 @@
- var sys = require('sys');
-var _ = require('underscore');
-var rclient = require('./redisclient');
-
-var global_action_list = "actions";
-var list_max_size = 1000;
-var listener;
-
